01fe2594b36f6ce122f19e38258e63740e05db84
Marco Caruso <marco.caruso98@gmail.com>
2024-06-25 01:05:18 +0200

    update

--- Sections/Section16/00_TheProblem/main.cpp
+++ Sections/Section16/00_TheProblem/main.cpp
@@ -22,25 +22,50 @@ void greetings(const Base &obj) {
     obj.say_hello();
 }
 
+
+class A 
+{
+    public: 
+    A() {f();}
+    virtual ~A(){f();}
+    virtual void f() const {std::cout<<1;}
+};
+class B : public A 
+{
+    public:
+    B() {f();}
+    virtual ~B(){f();}
+    virtual void f() const {std::cout<<2;}
+};
+
+
+
+
 int main() {
-    Base b;
-    b.say_hello();
+    // Base b;
+    // b.say_hello();
     
-    Derived d;
-    d.say_hello();
+    // Derived d;
+    // d.say_hello();
     
-    greetings(b);
-    greetings(d);
+    // greetings(b);
+    // greetings(d);
     
-    Base *ptr = new Derived();
-    ptr->say_hello();
+    // Base *ptr = new Derived();
+    // ptr->say_hello();
     
-    // Smart Pointers, wooow
-    std::unique_ptr<Base> ptr1 = std::make_unique<Derived>();
-    ptr1->say_hello();
+    // // Smart Pointers, wooow
+    // std::unique_ptr<Base> ptr1 = std::make_unique<Derived>();
+    // ptr1->say_hello();
     
-    delete ptr;
-    // there is no need to call delete ptr1, the smart pointer will handle that
+    // delete ptr;
+    // // there is no need to call delete ptr1, the smart pointer will handle that
+
+    const A* a = new B;
+    a->f();
+    delete a;
+
+    std::cout<<std::endl;
 
     return 0;
 }
