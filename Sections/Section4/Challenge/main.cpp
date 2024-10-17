311963aa3ebea4890430208b1faa08e05c135a88
Marco Caruso <marco.caruso98@gmail.com>
2024-10-17 10:41:56 +0200

    update

--- Sections/Section4/Challenge/main.cpp
+++ Sections/Section4/Challenge/main.cpp
@@ -6,4 +6,16 @@ int main(){
 	std::cin >> n;
 	std::cout << "Wow, " << n << " is my favourite number!" << std::endl;
 	return 0;
+
+	// int x = 3;
+	// bool b = true;
+	// uint y = 4;
+	// short z = 3;
+	// ushort w = 1;
+
+	// size_t p = sizeof(x);
+	// p = sizeof(b);
+	// p = sizeof(y);
+	// p = sizeof(z);
+	// p = sizeof(w);
 }
